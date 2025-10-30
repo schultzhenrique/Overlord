73845bafcdf715770d3285ed8e5ec91f3734b036
schultzhenrique <schultz.henrique@escola.pr.gov.br>
2025-10-30 09:29:45 -0300

    save

--- maiin.js
+++ maiin.js
@@ -3,7 +3,10 @@ console.log(avanca)
 
 avanca.forEach(button =>{
     button.addEventListener('click', function(){
-
+const atual = document.querySelector ('.ativo')
+  const proximoPasso = "passo" + this.getAttribute('data.proximo')
+  atual.classList.remove("ativo")
+  document.getElementById(proximoPasso).classList.add('ativo')
     })
 }
 
